ff8ea3ead844bfb65c6a9dc710b6ca25433887a8
Lupin0 <50870233+Lupin0@users.noreply.github.com>
2020-08-25 12:38:49 +0900

    leetcode
    회문관련 문제라 쉬울줄 알았다가 겨우 풀었다...

---
+++ leetcode/9/lupin.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    return x === Number(x.toString().split("").reverse().join(""));
+};
+//splite 로 배열을 쪼갠 후 
+//revsere 로 뒤집고
+//join으로 문자열로 다시 변경
+//Number로 값을 받아온다
+//그 후 x 값과 비교하여 참인지 거짓인지 판별
