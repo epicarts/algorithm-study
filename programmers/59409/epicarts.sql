eaae7cac7cf0ee87a9375483d42cde6495442e2a
YoungHo Choi <0505zxc@gmail.com>
2020-09-05 22:41:24 +0900

    프로그래머스 59409 SQL 문제

---
+++ programmers/59409/epicarts.sql
@@ -0,0 +1,7 @@
+SELECT ANIMAL_ID, NAME,
+   (CASE 
+        WHEN SEX_UPON_INTAKE LIKE 'Neutered%' OR SEX_UPON_INTAKE LIKE 'Spayed%' 
+        THEN 'O'
+        ELSE 'X'
+    END) AS '중성화'
+FROM ANIMAL_INS
