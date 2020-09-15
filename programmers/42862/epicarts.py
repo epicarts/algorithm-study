2efa2010820f8dcfcba88026d2bbb75ea7774032
YoungHo Choi <0505zxc@gmail.com>
2020-09-16 04:14:22 +0900

    제출 성공한 코드 업로드

--- programmers/42862/epicarts.py
+++ programmers/42862/epicarts.py
@@ -1,41 +1,20 @@
 def solution(n, lost, reserve):
     count = 0
-
-    for lo in lost:  # 잃어버린 인원체크
-
-        # 자기자신이 여벌의 옷을 가지고 있는가?
-        if lo in reserve:
-            reserve.remove(lo)
-            continue
+    losted = list(set(lost) - set(reserve))
+    reserved = list(set(reserve) - set(lost))
+    for lo in losted:  # 잃어버린 인원체크
 
         # 앞 사람이 여벌의 옷을 가지고 있는가 ?
-        if (lo - 1) in reserve:
-            reserve.remove(lo - 1)
+        if (lo - 1) in reserved:
+            reserved.remove(lo - 1)
             continue
 
         # 뒤 사람이 여벌의 옷을 가지고 있는가 ?
-        if (lo + 1) in reserve:
-            reserve.remove(lo + 1)
+        if (lo + 1) in reserved:
+            reserved.remove(lo + 1)
             continue
-
         count += 1
     return n - count
-
-def solution2(n, lost, reserve):
-    arr = [1] * n
-    for l in lost:
-        arr[l - 1] -= 1
-    for r in reserve:
-        arr[r - 1] += 1
-
-    for i in range(n - 1):
-        if arr[i] == 0 and arr[i + 1] == 2:
-            arr[i] = 1
-            arr[i + 1] = 1
-        if arr[i] == 2 and arr[i + 1] == 0:
-            arr[i] = 1
-            arr[i + 1] = 1
-    return n - arr.count(0)
 
 
 lost = [2, 5]
@@ -54,4 +33,3 @@ reserve = [28, 91, 42]
 # 내 정답: 100
 
 print(solution(n, lost, reserve))
-print(solution2(n, lost, reserve))
