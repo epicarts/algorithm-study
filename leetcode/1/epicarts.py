ebc0f1e5de171dec5fd510b1d8c57a319a23c05e
YoungHo Choi <0505zxc@gmail.com>
2021-03-10 18:51:27 +0900

    두번째 문제 풀이

--- leetcode/1/epicarts.py
+++ leetcode/1/epicarts.py
@@ -1,6 +1,24 @@
+# class Solution:
+#     def twoSum(self, nums, target):
+#         for i in range(len(nums)):
+#             for j in range(i + 1, len(nums)):
+#                 if nums[i] + nums[j] == target:
+#                     return [i, j]
+
 class Solution:
     def twoSum(self, nums, target):
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        nums_map = {}
+        for i, num in enumerate(nums): # O(n)
+            nums_map[num] = i # 키와 인덱스를 딕셔너리로.
+
+        for i, num in enumerate(nums):
+            if target - num in nums_map: # O(1)  => 해시 테이블.
+                if i != nums_map[target - num]: # 자기 자신은 제외해야함.
+                    return [i, nums_map[target - num]] # 현재 인덱스와 맵에 인덱스르 조회
+
+
+nums = [2,7,11,15]
+target = 9
+# output: [0,1]
+
+
