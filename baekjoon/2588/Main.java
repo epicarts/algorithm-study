9a3eb01d03a8c9bb8e4bdffcaa72aed97f8163cf
unknown <rlarudgkswkd@gmail.com>
2021-09-27 09:15:35 +0900

    해결

--- baekjoon/2588/Main.java
+++ baekjoon/2588/Main.java
@@ -1,31 +1,24 @@
 import java.util.Scanner;
 
-public class Main2 {
+public class Main {
    public static void main(String[] args) {
-       int A=0;
-       int B=0;
+       int A = 0;
+       int B = 0;
        int num = 0;
        int j = 1;
-       
+
        Scanner sc = new Scanner(System.in);
        A = sc.nextInt();
        B = sc.nextInt();
-       
-       
+
        String B1 = Integer.toString(B);
        String[] strB = B1.split("");
-       
-       for(int i=strB.length-1;i>=0;i--) {
-    	   System.out.println(A*Integer.parseInt(strB[i]));
-    	   num += A*Integer.parseInt(strB[i])*j;
-    	   j=j*10;
+
+       for (int i = strB.length - 1; i >= 0; i--) {
+           System.out.println(A * Integer.parseInt(strB[i]));
+           num += A * Integer.parseInt(strB[i]) * j;
+           j = j * 10;
        }
        System.out.println(num);
-       
-       
-       
-       
-       
    }
-
 }
