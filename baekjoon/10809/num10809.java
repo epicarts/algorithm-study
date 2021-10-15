4e466e91e8ebeb2cba2f2f684f4f86e209acdd2f
Crazy_Kyeong <48428850+rlarudgkswkd@users.noreply.github.com>
2021-10-15 09:13:37 +0900

    Update num10809.java
    로직 수정

--- baekjoon/10809/num10809.java
+++ baekjoon/10809/num10809.java
@@ -5,37 +5,32 @@ import java.util.Scanner;
 
 public class num10809 {
     public static void main(String[] args) {
-        int[] num_arr = {-1,-1,-1,-1,-1,
-                         -1,-1,-1,-1,-1,
-                         -1,-1,-1,-1,-1,
-                         -1,-1,-1,-1,-1,
-                         -1,-1,-1,-1,-1,
-                         -1};
+        int[] num_arr = new int[26];
+        for(int i = 0; i<num_arr.length;i++){
+            num_arr[i] = -1;
+        }
         ArrayList<Integer> arr = new ArrayList<Integer>();
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
         String[] str_array;
         int count = 0;
-        int temp = 0;
         str_array = str.split("");
         
         for (String s : str_array) {
             arr.add((int)s.charAt(0)-97);
         }
 
-        temp = arr.get(0);
-        num_arr[arr.get(0)] = count++;
-        for(int i=1;i<arr.size();i++){
-            if(temp!= arr.get(i)){
+        for(int i=0;i<arr.size();i++){
+            if(num_arr[arr.get(i)]==-1){
                 num_arr[arr.get(i)] = count;
-                
             }
-            temp = arr.get(i);
             count++;
         }
 
         for (int i : num_arr) {
             System.out.print(Integer.toString(i)+" ");
         }
+
+        sc.close();
     }
 }
