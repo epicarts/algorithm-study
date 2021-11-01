97f0fc7ce96d69abaf17c48b2d39c37c59c88d16
smuhsh <smu_hsh@naver.com>
2021-11-01 23:02:23 +0900

    smuhsh commit

--- 1330.java
+++ 1330.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class Main {
  
 	public static void main(String[] args) {
--- 9498.java
+++ 9498.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
         
